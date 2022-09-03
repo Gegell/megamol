e12cec3db85de1e2e8eef1e557e0c483dc499fc5
Alex Heller <st162467@stud.uni-stuttgart.de>
2022-09-03 22:36:24 +0200

    Ported voro++ external to vcpkg package

--- externals/voro++/patch.cmake
+++
@@ -1,7 +0,0 @@
-# Remove quotes from input path
-string(REPLACE "\"" "" VORO_CMAKE_FILE ${VORO_CMAKE_FILE})
-
-# Actually apply the fix
-file(READ ${VORO_CMAKE_FILE} filedata)
-string(REPLACE "src/voro++.hh" "src/*.hh" filedata "${filedata}")
-file(WRITE ${VORO_CMAKE_FILE} "${filedata}")
